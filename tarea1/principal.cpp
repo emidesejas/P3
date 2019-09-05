e010ac68a728926b609aa3fdb0984d3478fa7aa4
Emiliano <emidesejas@gmail.com>
2019-09-05 18:22:05 -0300

    Work with new ideas

--- tarea1/principal.cpp
+++ tarea1/principal.cpp
@@ -5,14 +5,15 @@
 #include "include/grafo.h"
 #include "include/lista_pares.h"
 #include "include/lista.h"
+#include "include/cola.h"
 
-struct level{
+struct butterfly{
     nat id;
-    Lista elements;
-    level *next;
+    butterfly *prev;
+    butterfly *next;
 };
 
-typedef level *bfsLevels;
+typedef butterfly *ButterfliesList;
 
 int main(){
     nat n, m, e1, e2;
@@ -39,15 +40,43 @@ int main(){
 bool consistenteMaximal(Grafo G) { // Quién dice que perder lógica no deja secuelas?
     bool discovered[cantidad_vertices(G)];
     bool label[cantidad_vertices(G)];
-    for(int i = 0; i < cantidad_vertices(G); i++) {
+    ButterfliesList Butterflies[cantidad_vertices(G)];
+    ButterfliesList auxList = new butterfly();
+    auxList -> id = 0;
+    auxList -> next = NULL;
+    auxList -> prev = NULL;
+    discovered[0] = false;
+
+    Butterflies[0] = auxList;
+    ButterfliesList cursor = auxList;
+    for(int i = 1; i < cantidad_vertices(G); i++) {
         discovered[i] = false;
+        cursor -> next = new butterfly();
+        cursor -> next -> prev = cursor;
+        cursor = cursor -> next;
+        cursor -> id = i;
+        Butterflies[i] = cursor;
     }
-    bfsLevels levels = new level;
-    levels -> id = 0; // No es necesario, se borrará en futuras versiones (es el numero del nivel)
-    levels -> elements = crear_lista();
-    insertar(0, levels->elements);
-    while(!es_vacia_lista(levels -> elements)){
+    cursor -> next = NULL;
+
+    //JMMM DUDOSO TODO ESTO ^^^^^^^
+
+
+    //bfsLevels levels = new level;
+    //levels -> id = 0; // No es necesario, se borrará en futuras versiones (es el numero del nivel)
+    //levels -> elements = crear_cola();
+    //encolar(0, levels -> elements);
+    Cola elements = crear_cola();
+    encolar(0, elements);
+    killButterfly(0, Butterflies, auxList);
+    while(!es_vacia_cola(elements)){
+        Lista_pares incidents = adyacentes(frente(elements), G);
+        discovered[frente(elements)] = true;
+
+
+
         // Basicamente crear nuevo nodo de bfs levels con los nodos que son incidentes a v en el grafo (usar pila para ir haciendo pop de cada nodo y poder irlos agregando)
+
     }
     
 }
@@ -57,4 +86,18 @@ void bfs(nat node, Grafo G, bool *discovered){
 
 }
 
+void killButterfly(nat id, ButterfliesList *Array, ButterfliesList List){ //Se me fue de las manos esto pero sino no me aseguro el O(1)
+    if(Array[id] != NULL){
+        if(Array[id] -> prev != NULL){
+            Array[id] -> prev -> next = Array[id] -> next;
+        } else {
+            List = Array[id] -> next;
+        }
+        if(Array[id] -> next != NULL){
+            Array[id] -> next -> prev = Array[id] -> prev;
+        }
+        delete Array[id];
+        Array[id] = NULL;
+    }
+}
 // HACER FUNCIONES PARA MANIPULAR BFSLEVELS, DESTRUIR MOVERSE ETC.
