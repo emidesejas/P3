585daab62bca4ee0564124f4a873d2deb72e01f2
emidesejas <emidesejas@gmail.com>
2019-11-02 18:19:42 -0300

    Everything working except for timeout error

--- tarea3/principal.cpp
+++ tarea3/principal.cpp
@@ -19,10 +19,17 @@
 #define MAX_LONG_PALABRA 80
 #define MAX_NOMBRE_ARCHIVO 80
 
+struct nodoLista{
+    int dato;
+    nodoLista *sig;
+};
+
+typedef nodoLista *Lista;
+
 struct nodo_texto{
-    char palabra[MAX_LONG_PALABRA];
     int largo;
     nodo_texto *siguiente;
+    char palabra[MAX_LONG_PALABRA]; //[MAX_LONG_PALABRA]
 };
 
 struct mejor_palabra{
@@ -40,6 +47,16 @@ typedef nodo_texto* texto;
 void procesar_palabra(texto &fuente, texto &cursor, char *palabra);
 
 void borrar_texto(texto &fuente);
+
+void agregarNodoLista(Lista &l, int elem);
+
+void destruirLista(Lista &l);
+
+int primeroLista(Lista l);
+
+void borrarPrimeroLista(Lista &l);
+
+void imprimir(char *s);
 
 int main () {
     int L; // largo de la linea
@@ -60,11 +77,7 @@ int main () {
     texto cursor = fuente;
     int cantidad_de_palabras = 0;
     int  res_lectura  = fscanf(descriptor, "%s",  palabra);
-    /* if(res_lectura != EOF){
-        cantidad_de_palabras++;
-    } */
-    //printf("%s ",palabra);
-    //procesar_palabra(fuente, cursor, palabra);
+   
     
     while (res_lectura  != EOF) {
         // procesar palabra
@@ -80,95 +93,86 @@ int main () {
 
     //printf("%d\n", cantidad_de_palabras);
     if(cantidad_de_palabras){
-        int i = 0;
-        /* info_palabra info_palabras[cantidad_de_palabras];
-        cursor = fuente;
-        
-        while(cursor != NULL){
-            info_palabras[i].largo = cursor -> largo;
-
-            info_palabras[i].palabra = cursor -> palabra; //NOT SURE, quiero el puntero del array palabra peeeero
-            cursor = cursor -> siguiente;
-        } */
-
-        /* int opt[cantidad_de_palabras];
-        opt[0] = 0; */
-
-        mejor_palabra S[cantidad_de_palabras];
-        cursor = fuente;
-        i = 0;
-        for(int i = 0; i<cantidad_de_palabras; i++){
-            S[i].indice = i;
-            S[i].valor = (L - (cursor -> largo))*(L - (cursor -> largo));
-            cursor = cursor -> siguiente;
+        int *s[cantidad_de_palabras];
+        for(int i = 0; i < cantidad_de_palabras; i++){
+            s[i] = new int[cantidad_de_palabras - i];
         }
-        
-        /* for(int i = 0; i < cantidad_de_palabras; i++){
-            S[i].indice = i;
-            S[i].valor = L - (info_palabras[i].largo);
-        } */
 
         cursor = fuente;
-        i = 0;
-        int j = 0;
-        while(cursor != NULL){
-            int Ci = cursor -> largo;
-            texto cursorAd = cursor -> siguiente;
-            j = i + 1;
-            while(cursorAd != NULL){
-                Ci = Ci + 1 + cursorAd -> largo;
+        texto cursorAd;
+        
+
+        //Cargamos los valores de S, desperdiciamos mucha memoria, en el futuro deberia usar otra estructura que no sea una matriz
+        for(int i=0; i<cantidad_de_palabras; i++){ //tenia un -1 cantidad de palabras, I dont know why
+            int Ci = cursor->largo;
+            s[i][0] = L - Ci;
+            cursorAd = cursor->siguiente;
+            for(int j = 1; j < cantidad_de_palabras - i; j++){
+                Ci = Ci + 1 + cursorAd ->largo; //el +1 es el espacio entre las palabras
                 if(L >= Ci){
-                    if(S[i].valor > (L - Ci)*(L - Ci)){
-                        S[i].valor = (L - Ci)*(L - Ci);
-                        S[i].indice = j;
-                    }
+                    s[i][j] = L - Ci;
                 } else {
-                    break; //TURBINA, lo cambio despues, quiero que funcione
+                    s[i][j] = -1;
                 }
                 cursorAd = cursorAd -> siguiente;
-                j++;
             }
             cursor = cursor -> siguiente;
-            i++;
         }
 
-        /* for(int i = 0; i < cantidad_de_palabras; i++){
-            int Ci = info_palabras[i].largo; //Ci es el costo total de todas las palabras juntas, con los espacios
-            for(int j = i + 1; j < cantidad_de_palabras; j++){ //La palabra i ya fue computada en el for anterior
-                Ci = Ci + 1 + info_palabras[j].largo; //El +1 es el espacio en medio de las palabras, no se agrefa el espacio del final
-                if(L >= Ci){
-                    if(S[i].valor > L - Ci){
-                        S[i].valor = L - Ci;
-                        S[i].indice = j;
-                    }
-                } else {
-                    break; //TURBINA, lo cambio despues, quiero que funcione
+        
+        int opt[cantidad_de_palabras + 1];
+        int indice_opt[cantidad_de_palabras + 1];
+        opt[0] = 0;
+        indice_opt[0] = 0;
+        for(int i = 1; i <= cantidad_de_palabras; i++){
+            int min = ( s[i-1][0] * s[i-1][0] ) + opt[i-1]; //tenemos que inciaizar con algo min, no es el primero, seria el ultimo valor calculado del for
+            indice_opt[i] = i - 1;
+            for(int j = 1; j < i; j++){
+                if(s[j-1][i-j] != -1 && (s[j-1][i-j]*s[j-1][i-j]) + opt[j-1] <= min){
+                    min = ( s[j-1][i-j] * s[j-1][i-j] ) + opt[j-1];
+                    indice_opt[i] = j - 1; //Este es el j que nos sirve para i
                 }
-                
             }
-        } */
-
-        cursor = fuente;
-        i = 0;
-        j = 0;
-        while(cursor != NULL){
-            printf("%s", cursor -> palabra);
-            if(S[i].indice == j){
-                printf("\n");
-                i = j + 1;
-                j = i;
-            } else if(cursor -> siguiente != NULL) {
-                printf(" ");
-                j++;
-            }
-            cursor = cursor -> siguiente;
+            opt[i] = min;
         }
 
+
+        
+        Lista optimos = NULL;
+        agregarNodoLista(optimos, cantidad_de_palabras);
+        int i = indice_opt[cantidad_de_palabras]; //Arranca por el ultimo, queremos saber hasta donde imprimir en cada linea
+        
+        while(i > 0){ //Vamos hacia atras hasta llegar a 0
+            agregarNodoLista(optimos, i);
+            i = indice_opt[i];
+            //printf("%d\n",i);
+        }
+
+        int actual = 0;
+        cursor = fuente;
+        
+        while(actual < cantidad_de_palabras){
+            int renglonActual = primeroLista(optimos);
+            borrarPrimeroLista(optimos);
+            while(actual < renglonActual - 1){
+                printf("%s ", cursor -> palabra);
+                cursor = cursor -> siguiente;
+                actual++;
+            }
+            
+            printf("%s\n", cursor -> palabra); //Necesitamos diferenciar el ultimo caso por el espacio y el enter
+            cursor = cursor -> siguiente;
+            actual++;
+        }
+        
+        for(int i = 0; i < cantidad_de_palabras; i++){ //borrar la matriz
+            delete[] s[i];
+        }
 
     }
 
     borrar_texto(fuente);
-    
+
     // Se debe cerrar el arhivo
     fclose(descriptor);
     // procesar palabras leidas e imprimir
@@ -196,12 +200,48 @@ void procesar_palabra(texto &fuente, texto &cursor, char *palabra) {
         cursor -> siguiente = new nodo_texto;
         cursor = cursor -> siguiente;
     }
+
     int n = 0;
     while (palabra[n] != '\0'){ //Medimos el largo de la palabra, en C todas las palabras terminan con el caracter especial \0
         cursor -> palabra[n] = palabra[n];
         n++;
     }
-    cursor -> palabra[n + 1] = '\0';
+
+    cursor -> palabra[n] = '\0';
     cursor -> largo = n;
     cursor -> siguiente = NULL;
+}
+
+void agregarNodoLista(Lista &l, int elem){
+    if(l == NULL){
+        l = new nodoLista;
+        l -> dato = elem;
+        l -> sig =  NULL;
+    } else {
+        nodoLista *nuevo = new nodoLista;
+        nuevo -> dato = elem;
+        nuevo -> sig = l;
+        l = nuevo;
+    }
+}
+
+int primeroLista(Lista l){ //Se asume lista no vacia
+    return l -> dato;
+}
+
+void borrarPrimeroLista(Lista &l){
+    if(l != NULL){
+        nodoLista *temp = l ->sig;
+        delete l;
+        l = temp;
+    }
+}
+
+void destruirLista(Lista &l){
+    if(l != NULL){
+        if(l -> sig != NULL){
+            destruirLista(l->sig);
+        }
+        delete l;
+    }
 }
